66d4c8119c90eda5b9853aeb17a1cffc620d2acc
Alex Lewis <ebcomalex@gmail.com>
2011-09-06 10:36:44 -0400

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gassouthbroadway::Application.config.secret_token = 'faba2a11f70dfd59b5e548e11ef706065129010aeb15ef18459dca8fb313fa562c55d3d7220f38daef8515023971712e287a45700933247b72c8e2971fb51ae8'
